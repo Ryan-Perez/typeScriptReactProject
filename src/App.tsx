cd185bce2c7f8e436f9720edda684b3ea89629a5
ryan perez <perezryanm@gmail.com>
2022-07-07 11:06:37 -0700

    finished react typescript project

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from "./components/list"
+import AddToList from './components/AddToList';
 
-interface IState {
+export interface IState {
   people: {
     name: string
     age: number
@@ -14,12 +15,20 @@ interface IState {
 
 function App() {
 
-  const [people, setpeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>([
+  {
+    name: 'LeBron James',
+    url: 'https://upload.wikimedia.org/wikipedia/commons/b/bf/LeBron_James_-_51959723161_%28cropped%29.jpg',
+    age: 36,
+    note: 'Allergic to staying on the same team'
+  }
+  ])
 
   return (
     <div className="App">
-      <h1>People Invited to my Part</h1>
+      <h1>People Invited to my Party</h1>
       <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
